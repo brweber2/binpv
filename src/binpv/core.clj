e2c6b5e26a51bd676a6a2737d42e3b3ecd3557b9
Bryan Weber <brweber2@yahoo.com>
2011-11-11 16:10:31 -0500

    starting to add some real test nonsense

--- src/binpv/core.clj
+++ src/binpv/core.clj
@@ -1,1 +1,54 @@
 (ns binpv.core)
+
+(defn get-sections [sections id]
+    (filter id sections))
+
+(defn get-section [sections id]
+    (take 1 (get-sections sections id)))
+
+(def section-keys #{:ID :FIXED_LENGTH :DEPENDENT_FIXED_LENGTH :VARIABLE_LENGTH :DEPENDENT_FUN :ENUM_FUN :DESCRIPTION})
+
+
+(defn section 
+	"Valid section keys:
+	:ID
+		Can be a string, symbol or keyword.
+	
+	; only one of the following:
+	:FIXED_LENGTH
+	:DEPENDENT_FIXED_LENGTH
+		Takes a function that returns the length of the section.  
+		The function is passed a sequence the sections parsed so far.
+	:VARIABLE_LENGTH
+		Takes a function that returns true when it is time to stop reading the section.
+		The function takes one chunk and a context.  It must return a seq of a boolean and a context map.
+		The context map allows state to be passed from chunk to chunk since they are read one at a time.
+
+	:DEPENDENT_FUN 
+		Function called that will determine if this section is skipped or present.
+		The function takes a sequence of sections that have been parsed so far.
+	:ENUM_FUN
+		Function that takes a sequence of the chunks read.
+
+	:DESCRIPTION"
+	[& section-info]
+	; todo add post condition that validates keys and values?
+	(apply hash-map section-info))
+
+(defn parse-section [stream section]
+	)
+
+(defn parse-binary [binary-stream binary-protocol]
+	(map (partial (parse-section binary-stream) (:sections binary-protocol))))
+
+(defn visualize-section [section]
+	)
+
+(defn visualize-binary [parsed-binary]
+	)
+
+(defn binp [basis-keyword, basis & sections]
+	^{basis-keyword basis} sections)
+
+(defmacro defbinp [name-to-def, basis-keyword, basis & sections ]
+	`(def ~name-to-def (binp ~basis-keyword ~basis ~@sections)))
--- test/binpv/test/core.clj
+++ test/binpv/test/core.clj
@@ -2,8 +2,7 @@
   (:use [binpv.core])
   (:use [clojure.test]))
 
-(deftest replace-me 
-  (is false "No tests have been written."))
+(def THE_FILE "/tmp/foo.txt")
 
 
 ; create valid binary file for our protocol
@@ -11,41 +10,44 @@
 ; parse binary file
 ; visualize binary file
 
-; helper funs that go into code
+(defn get-bytes [n]
+    (take n (map byte (cycle (range -127 128)))))
 
-(defn get-section [sections id]
-    (take 1 (get-sections sections id)))
+(deftest foo
+        (spit THE_FILE (String. (byte-array (get-bytes 1000))))
+        (def q (slurp THE_FILE))
+        (is true (= (alength (.getBytes q)) 1000))
+        )
 
-(defn get-sections [sections id]
-    (filter id sections))
+
 
 ; protocol specific helper funs
-(defn includes-private-key? [a-byte]
-    (case a-byte
+(defn includes-private-key? [the-bytes]
+    ; todo add precondition that the size of the-bytes is one?
+    (case (first (Character/toChars (first the-bytes)))
         \v :private
         \b :public))
 
 (defn private-key-present? [sections]
-    (= (get-section sections :INCLUDES-PRIVATE-KEY) :private))
+    (= (get-section sections :INCLUDES_PRIVATE_KEY) :private))
 
 (defn public-key-length [sections]
-    (Integer/parseInt (get-section sections :PUBLIC-KEY-LENGTH)))
+    (Integer/parseInt (get-section sections :PUBLIC_KEY_LENGTH)))
 
 (defn stop-at-fun [next-byte context]
-    (= \; next-byte))
+    [(= \; next-byte) context])
     
 (defn all-done [some-bytes]
     (= (str some-bytes) "AD"))
 
-(defbinp ^{:BASIS :BYTE} [
-    (section :FIXED_LENGTH 2, :DESCRIPTION "Key Token Identifier")
-    (section :ID :INCLUDES-PRIVATE-KEY, :FIXED_LENGTH 1, :ENUM_FUN includes-private-key?) 
-    (section :FIXED_LENGTH 256, :DEPENDENT_FUN private-key-present?, :DESCRIPTION "Private Key") 
-    (section :ID :PUBLIC_KEY_LENGTH, :FIXED_LENGTH 2)
-    (section :ID :PUBLIC_KEY, :DEPENDENT_FIXED_LENGTH public-key-length)
-    (section :ID :THROW-AWAY, :VARIABLE_LENGTH stop-at-fun)
-    (section :ID :THE-END, :FIXED_LENGTH 2, :ENUM_FUN all-done))
-
+(defbinp key-token-format :BASIS :BYTE
+    (section :ID :KEY_TOKEN_ID,         :FIXED_LENGTH 2, :DESCRIPTION "Key Token Identifier")
+    (section :ID :INCLUDES_PRIVATE_KEY, :FIXED_LENGTH 1, :ENUM_FUN includes-private-key?) 
+    (section :ID :PRIVATE_KEY,          :FIXED_LENGTH 256, :DEPENDENT_FUN private-key-present?) 
+    (section :ID :PUBLIC_KEY_LENGTH,    :FIXED_LENGTH 2)
+    (section :ID :PUBLIC_KEY,           :DEPENDENT_FIXED_LENGTH public-key-length)
+    (section :ID :THROW_AWAY,           :VARIABLE_LENGTH stop-at-fun)
+    (section :ID :THE_END,              :FIXED_LENGTH 2, :ENUM_FUN all-done))
 
 
 ; repeat, but for invalid binary file
